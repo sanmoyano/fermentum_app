213ea1bef0a62fda68c44ac60d673cbbbbef024e
sanmoyano <smoyano.di@gmail.com>
2022-02-21 15:57:46 -0300

    subida al repo

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+const NavBar = () => {
+    return (
+        <header>
+            <nav className="nav-bar">
+                <h1>Fermentum Faber</h1>
+                <div className="nav-bar-items">
+                    <ul>
+                        <li>Inicio</li>
+                        <li>Cervezas</li>
+                        <li>Tienda</li>
+                        <li>Contacto</li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
