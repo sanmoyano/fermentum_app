34fd22793e2de824a0ba266c6f94a9cb9fca4226
sanmoyano <smoyano.di@gmail.com>
2022-03-25 23:09:05 -0300

    agregando firebase

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -2,33 +2,43 @@ import ItemList from "./ItemList"
 import { useState, useEffect } from "react"
 import { toast } from "react-toastify"
 import { useParams } from "react-router-dom"
-import itemsIniciales from "./estilosIniciales.js"
+// import itemsIniciales from "./estilosIniciales.js"
+import { db } from "./firebase"
+import { collection, getDocs } from "firebase/firestore"
 
 
 
 const ItemListContainer = () => {
-    
-    const [loading, setLoading] = useState(true) 
+
+    const [loading, setLoading] = useState(true)
     const [items, setItems] = useState([])
-    const {idCategoria} = useParams()
-    
-    const getItems = () => {
-        return new Promise ((resolve, reject) => {
-            setTimeout(() => {resolve(itemsIniciales)}, 1000)
-        })
-    }
+    const { idCategoria } = useParams()
+
+    // const getItems = () => {
+    //     return new Promise ((resolve, reject) => {
+    //         setTimeout(() => {resolve(itemsIniciales)}, 1000)
+    //     })
+    // }
 
     useEffect(() => {
-        getItems()
-        .then(promesaRespuesta => setItems(promesaRespuesta))
-        .catch(() => toast.error("Error cargando catalogo de productos"))
-        .finally(() => {setLoading(false)})
+
+        const itemCollection = collection(db, "items")
+        const consulta = getDocs(itemCollection)
+        consulta
+            .then(promesaRespuesta => setItems(promesaRespuesta))
+            .catch(() => toast.error("Error cargando catalogo de productos"))
+            .finally(() => { setLoading(false) })
+
+        // getItems()
+        // .then(promesaRespuesta => setItems(promesaRespuesta))
+        // .catch(() => toast.error("Error cargando catalogo de productos"))
+        // .finally(() => {setLoading(false)})
     }, [idCategoria])
 
-    if(loading) {
+    if (loading) {
         return <h1>Cargando...</h1>
     } else {
-        return<ItemList items={items} />
+        return <ItemList items={items} />
     }
 }
 
---
+++ src/components/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB88JYhJvR81Ltgv6qbXIbjDujZ8vDDssI",
+    authDomain: "fermentum-app.firebaseapp.com",
+    projectId: "fermentum-app",
+    storageBucket: "fermentum-app.appspot.com",
+    messagingSenderId: "474499944590",
+    appId: "1:474499944590:web:74078d36feef8d9bc939df"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
